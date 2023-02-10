cc0cc8f4b9685ee9a8cf575038a6e089fde703cb
unknown <kumarpritam.baral2000@gmail.com>
2023-02-10 17:16:57 +0530

    youtube downloader first commit

---
+++ ytdownloader/home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from pytube import *
+
+
+
+def youtube(request):
+    if request.method == 'POST':
+        try:
+            link = request.POST['link']
+            video = YouTube(link)
+            stream = video.streams.get_lowest_resolution()
+            stream.download()
+            return render(request, 'home.html',{'msg':'video downloaded'})
+        except:
+                return render(request, 'home.html', {'msg':'Video not downloaded or wrong url'})
+        
+    return render(request, 'home.html')
